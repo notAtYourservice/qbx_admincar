4a10d0175ec3d42f2aa3b7fbea52b3630ad14cc3
notAtYourservice <sofaidtullah16@gmail.com>
2025-08-21 02:46:38 -0400

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,11 @@
+-- Configuration file for admincar command
+
+config = {
+    -- Permission level required to use the admincar command
+    saveVehicle = 'admin',  -- Can be 'admin', 'god', or specific ace permission
+    
+    -- Additional configuration options can be added here
+    -- For example:
+    -- maxVehicles = 10,
+    -- allowDuplicates = false,
+}
